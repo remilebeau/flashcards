a061f31e3d27602d3a5ae3b131ba28f7191528f7
remilebeau <remilebeau90@gmail.com>
2024-04-05 20:09:20 -0400

    finished prototype

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FlashCard from "./components/FlashCard";
 
 function App() {
   return (
-    <main>
+    <main className="min-h-screen bg-black text-white max-w-md mx-auto">
       <FlashCard />
     </main>
   );
--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -25,24 +25,28 @@ const FlashCard = () => {
   }
 
   return (
-    <section>
-      <h1>{content}</h1>
-      {/* button to flip card */}
-      <button
-        onClick={() => {
-          setShowLangOne(!showLangOne);
-        }}
-      >
-        Flip
-      </button>
-      {/* button to draw next card */}
-      <button
-        onClick={() => {
-          fetchFlashCard();
-        }}
-      >
-        Next
-      </button>
+    <section className="flex flex-col items-center justify-center border border-red-500">
+      <section className="flex flex-col items-center justify-center">
+        <h1 className="text-3xl text-white">{content}</h1>
+        {/* button to flip card */}
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => {
+            setShowLangOne(!showLangOne);
+          }}
+        >
+          Flip
+        </button>
+        {/* button to draw next card */}
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => {
+            fetchFlashCard();
+          }}
+        >
+          Next
+        </button>
+      </section>
     </section>
   );
 };
