ac885b25e47eed56a1e2ecc28c1e3702e40264e0
remilebeau <remilebeau90@gmail.com>
2024-04-09 22:53:28 -0400

    split Draw button into Draw Random and Draw Next features

--- src/features/FlashCards/AddFlashCard.tsx
+++ src/features/FlashCards/AddFlashCard.tsx
@@ -15,30 +15,33 @@ const AddFlashCard = () => {
     setAnswer("");
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        className="text-black"
-        type="text"
-        placeholder="Question"
-        required
-        value={question}
-        onChange={(event) => setQuestion(event.target.value)}
-      />
-      <input
-        className="text-black"
-        type="text"
-        placeholder="Answer"
-        required
-        value={answer}
-        onChange={(event) => setAnswer(event.target.value)}
-      />
-      <button
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-        type="submit"
-      >
-        Add
-      </button>
-    </form>
+    <section className="bg-slate-900 mt-20 p-4 rounded-3xl">
+      <h2 className="text-3xl text-center mb-4">Add a Flash Card</h2>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <input
+          className="text-black mx-4 mb-2 w-1/2"
+          type="text"
+          placeholder="Question"
+          required
+          value={question}
+          onChange={(event) => setQuestion(event.target.value)}
+        />
+        <input
+          className="text-black mx-4 mb-2 w-1/2"
+          type="text"
+          placeholder="Answer"
+          required
+          value={answer}
+          onChange={(event) => setAnswer(event.target.value)}
+        />
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+    </section>
   );
 };
 
--- src/features/FlashCards/FlashCard.tsx
+++ src/features/FlashCards/FlashCard.tsx
@@ -25,6 +25,20 @@ const FlashCard = () => {
     };
     setQuestion(randomCard.question);
     setAnswer(randomCard.answer);
+    setShowAnswer(false);
+  };
+
+  const selectNextCard = () => {
+    // use the question to find the index of the current card
+    const currentCardIndex = flashCards.flashcards.findIndex(
+      (card) => card.question === question
+    );
+    // add 1 to that index to return the next card in the list || return the first card
+    randomCard =
+      flashCards.flashcards[currentCardIndex + 1] ?? flashCards.flashcards[0];
+    setQuestion(randomCard.question);
+    setAnswer(randomCard.answer);
+    setShowAnswer(false);
   };
 
   useEffect(() => {
@@ -33,31 +47,40 @@ const FlashCard = () => {
   }, [flashCards.flashcards]);
 
   return (
-    <section className="flex flex-col items-center justify-center border border-red-500">
-      <section className="flex flex-col items-center justify-center">
+    <section className="bg-slate-900 mt-20 p-4 rounded-3xl">
+      {/* card count */}
+      <h2 className="text-3xl text-white">Card Count: {cardCount}</h2>
+      <section className="grid grid-rows-2 items-center justify-center">
         <h1 className="text-3xl text-white">
           {showAnswer ? answer : question}
         </h1>
         {/* button to flip card */}
         <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          className="row-start-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-2 rounded"
           onClick={() => {
             setShowAnswer(!showAnswer);
           }}
         >
           Flip
         </button>
-        {/* button to draw next card */}
+        {/* button to draw random card */}
         <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          className="row-start-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-2 rounded"
           onClick={() => {
             selectRandomCard();
           }}
         >
-          Draw
+          Draw Random
         </button>
-        {/* show card count */}
-        <h1 className="text-3xl text-white">Card Count: {cardCount}</h1>
+        {/* button to draw next card */}
+        <button
+          className="row-start-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-2 rounded"
+          onClick={() => {
+            selectNextCard();
+          }}
+        >
+          Draw Next
+        </button>
       </section>
     </section>
   );
