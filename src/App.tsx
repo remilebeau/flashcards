c603b14e4309da1a1f3092d68376a29b26b58880
remilebeau <remilebeau90@gmail.com>
2024-04-26 12:40:31 -0400

    Revised UI of CardCount.tsx with ShadCN

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,15 @@ import { ThemeProvider } from "./components/theme-provider";
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <main className="mx-auto flex min-h-screen max-w-4xl flex-col sm:grid sm:grid-rows-2 sm:gap-8">
+      <main
+        className="mx-auto flex min-h-screen max-w-4xl flex-col sm:grid sm:grid-rows-2 sm:gap-8
+      "
+      >
         <AddFlashCards />
+        <section className="flex flex-col items-center">
+          <CardCount />
+        </section>
         <FlashCard />
-        <CardCount />
       </main>
     </ThemeProvider>
   );
