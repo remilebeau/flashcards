3605faba33b1ea731ee5879a905fe5a12ad1c917
remilebeau <remilebeau90@gmail.com>
2024-04-26 12:16:28 -0400

    Added ShadCN to project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import FlashCard from "./features/FlashCards/FlashCard";
 import AddFlashCards from "./features/FlashCards/AddFlashCards";
 import CardCount from "./features/FlashCards/CardCount";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <main className="mx-auto flex min-h-screen max-w-3xl flex-col bg-black text-white sm:grid sm:grid-rows-2 sm:gap-8">
-      {/* <section id="grid-container" className="sm:grid sm:grid-cols-2 sm:gap-4"> */}
-      <AddFlashCards />
-      <br className="my-8 sm:hidden" />
-      <FlashCard />
-      <br className="my-8 sm:hidden" />
-      <CardCount />
-      {/* </section> */}
-    </main>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="mx-auto flex min-h-screen max-w-3xl flex-col bg-black text-white sm:grid sm:grid-rows-2 sm:gap-8">
+        {/* <section id="grid-container" className="sm:grid sm:grid-cols-2 sm:gap-4"> */}
+        <AddFlashCards />
+        <br className="my-8 sm:hidden" />
+        <FlashCard />
+        <br className="my-8 sm:hidden" />
+        <CardCount />
+        {/* </section> */}
+      </main>
+    </ThemeProvider>
   );
 }
 
